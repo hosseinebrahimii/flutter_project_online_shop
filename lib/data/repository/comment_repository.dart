687d5a205ca96c62f44ea920b21f26b4c2411e89
Hossein Ebrahimi <hossein.ebr4himi@gmail.com>
2023-11-12 17:07:35 +0330

    created comment_repository.dart

---
+++ lib/data/repository/comment_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_project_online_shop/data/datasource/comment_datasource.dart';
+import 'package:flutter_project_online_shop/di/di.dart';
+import 'package:flutter_project_online_shop/models/comment.dart';
+import 'package:flutter_project_online_shop/models/product.dart';
+import 'package:flutter_project_online_shop/util/api_exception.dart';
+
+abstract class ICommentRepository {
+  Future<Either<String, List<Comment>>> repositoryGetCommentList(Product product);
+}
+
+class CommentRepository extends ICommentRepository {
+  final ICommentDataSource _dataSource = locator.get();
+  @override
+  Future<Either<String, List<Comment>>> repositoryGetCommentList(Product product) async {
+    try {
+      var response = await _dataSource.dataSourceGetCommentList(product);
+      return right(response);
+    } on ApiException catch (ex) {
+      return left(ex.message!);
+    } catch (ex) {
+      return left('خطای نامشخص هنگام دریافت نظرات');
+    }
+  }
+}
